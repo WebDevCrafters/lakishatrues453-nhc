8da339f22f32c7291cd2a86d5de226bd94ae34ed
Eissa Faheem <eissafaheem@gmail.com>
2024-06-02 15:50:33 +0530

    feat: navbar header completed

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 const navbar = document.getElementsByTagName("nav")[0];
 
 
-fetch('./navbar.html')
-    .then(response => response.text())
-    .then(data => {
-        navbar.innerHTML = data;
-    })
-    .catch(error => console.error('Error fetching navbar:', error));
+
+
+
+const menu = document.querySelector(".menu")
+const routes = document.querySelector(".routes")
+const menuOpenSrc = "./assets/menu.svg";
+const menuClosedSrc = "./assets/close.svg";
+const menuIcon = document.getElementById("menuIcon");
+
+
+menu.addEventListener("click", () => {
+    if (routes.style.display === "none" || routes.style.display === "") {
+        routes.style.display = "block";
+        menuIcon.src = menuClosedSrc;
+
+    } else {
+        menuIcon.src = menuOpenSrc;
+        routes.style.display = "none";
+    }
+})
