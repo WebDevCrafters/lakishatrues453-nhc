274887b16e652942b5a6ebf94fdfdb32d2383572
rizwankazi661 <rizwankazi661@gmail.com>
2024-06-03 20:20:10 +0530

    feat: home added

--- script.js
+++ script.js
@@ -22,20 +22,30 @@ menu.addEventListener("click", () => {
     }
 })
 
+function handleResize() {
+    var width = window.innerWidth;
+    var height = window.innerHeight;
+    if (width >= 1000) {
+        console.log("Triggered");
+    }
+    if (width <= 950) {
+        var homeBanner = document.getElementById('homeBanner');
+        homeBanner.style.backgroundImage = "url('../assets/old-and-young-lady-2.jpg')";
+    }
+}
 
+handleResize();
 
-if (window.innerWidth >= 800) {
-    window.addEventListener('scroll', function () {
+window.addEventListener('resize', handleResize);
+
+window.addEventListener('scroll', function () {
+    var width = window.innerWidth;
+    if (width >= 800) {
         var scrollPosition = window.scrollY;
         var homeBanner = document.getElementById('homeBanner');
         var overlay = document.querySelector('.overlay');
         var opacity = Math.min(scrollPosition * 0.001, 1);
         overlay.style.backgroundColor = `rgb(51, 211, 199, ${opacity})`;
         homeBanner.style.backgroundSize = 'calc(100% + ' + scrollPosition * 1.5 + 'px)';
-    });
-
-    let index = 0;
-    const slides = document.querySelector('.slides');
-    const slideWidth = slides.clientWidth;
-    const totalSlides = slides.childElementCount;
-}
+    }
+});
