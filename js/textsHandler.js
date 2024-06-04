b6e346218c87e67ed25f1c52f824b9386192b02e
rizwankazi661 <rizwankazi661@gmail.com>
2024-06-04 23:20:37 +0530

    Localization

--- js/textsHandler.js
+++ js/textsHandler.js
@@ -1,9 +1,101 @@
 
 function handleText(data) {
+
+    // Home Page: 
     $(function () {
-        console.log("name is ", data.brandName.firstLine)
-        $(".first").text(data.brandName.firstLine)
-        $(".second").text(data.brandName.secondLine)
+        $(".first").text(data.navbar.brandName)
+        $(".second").text(data.navbar.brandSubHeading)
+        $(".banner").text(data.banner.imageCaption)
+    })
+
+    // Experience Section
+    $(function () {
+        $(".experience-heading").text(data.experience.heading)
+        $(".experience-content").text(data.experience.content)
+    })
+
+    // Features Section
+    $(function () {
+        $(".heading_1").text(data.features.heading_1)
+        $(".heading_2").text(data.features.heading_2)
+        $(".heading_3").text(data.features.heading_3)
+        $(".content_1").text(data.features.content_1)
+        $(".content_2").text(data.features.content_2)
+        $(".content_3").text(data.features.content_3)
+    })
+
+    // What we do Section
+    $(function () {
+        $(".what-we-do-heading").text(data.whatWeDo.heading)
+        $(".what-we-do-content").text(data.whatWeDo.content)
+    })
+
+    // Who are we Section
+    $(function () {
+        $(".who-are-we-heading").text(data.whoAreWe.heading)
+        $(".who-are-we-content").text(data.whoAreWe.content)
+    })
+
+    // Our Services Section
+    $(function () {
+        $(".service-heading").text(data.ourServices.heading)
+        $(".service-1").text(data.ourServices.service_1)
+        $(".service-2").text(data.ourServices.service_2)
+        $(".service-3").text(data.ourServices.service_3)
+        $(".service-4").text(data.ourServices.service_4)
+        $(".service-5").text(data.ourServices.service_5)
+        $(".service-6").text(data.ourServices.service_6)
+        $(".service-7").text(data.ourServices.service_7)
+        $(".service-8").text(data.ourServices.service_8)
+        $(".service-9").text(data.ourServices.service_9)
+    })
+
+    // Contacts Section
+    $(function () {
+        $(".contacts-heading").text(data.contacts.heading)
+        $(".contacts-content").text(data.contacts.content)
+    })
+
+    // Footer Section
+    $(function () {
+        $(".footer-brand-name").text(data.footer.brandName)
+        $(".footer-address").text(data.footer.address)
+        $(".footer-telephone").text(data.footer.phone)
+        $(".footer-email").text(data.footer.emailInfo)
+        $(".footer-copyright").text(data.footer.copyRight)
+    })
+
+    // About Page
+    $(function () {
+        $(".about-us-heading").text(data.aboutUs.heading)
+        $("#about-us-content").text(data.aboutUs.content)
+    })
+
+    // Our Mission Section
+    $(function () {
+        $(".our-mission-heading").text(data.ourMission.heading)
+        $(".our-mission-content").text(data.ourMission.content)
+    })
+
+    // Our Vision Section
+    $(function () {
+        $(".our-vision-heading").text(data.ourVision.heading)
+        $(".our-vision-content").text(data.ourVision.content)
+    })
+
+    // Core Value Section
+    $(function () {
+        $(".core-value-heading").text(data.coreValue.heading)
+        $(".core-value-1").text(data.coreValue.coreValue_1)
+        $(".core-value-2").text(data.coreValue.coreValue_2)
+        $(".core-value-3").text(data.coreValue.coreValue_3)
+        $(".core-value-4").text(data.coreValue.coreValue_4)
+        $(".core-value-5").text(data.coreValue.coreValue_5)
+        $(".core-value-6").text(data.coreValue.coreValue_6)
+        $(".core-value-7").text(data.coreValue.coreValue_7)
+        $(".core-value-8").text(data.coreValue.coreValue_8)
+        $(".core-value-9").text(data.coreValue.coreValue_9)
+        $(".core-value-10").text(data.coreValue.coreValue_10)
     })
 }
 
--- text.js
+++ text.js
@@ -1,8 +1,151 @@
 const textJson = {
-    "brandName": {
-        "firstLine": "Hello ",
-        "secondLine": "Bhai kya"
+
+    // Navbar Section:
+    "navbar": {
+        "brandName": "NURTURED",
+        "brandSubHeading": "HOME CARE"
+    },
+
+    // Home Page:
+    "banner": {
+        "imageCaption": `NHC is now MEDICAID accredited! let your loved ones care for you and get paid!`
+    },
+
+    // Experience Section:
+    "experience": {
+
+        "heading": "EXPERIENCE EXCEPTIONAL COMPASSION AND CARE",
+
+        "content":
+            "At Nurtured Home Care-Indiana, we provide a comprehensive range of home support services with empathetic caregivers, personalized care plans, skilled and competent staff, respect for independence and dignity, emotional support, clear communication, continuity of care, and always going the extra mile. We believe in enhancing the quality of life for our clients, and strive to provide exceptional care and support every step of the way."
+    },
+
+    // Features Section:
+    "features": {
+
+        "heading_1": "PERSONALIZED",
+
+        "content_1":
+            "We believe in the power of personalized care. We understand that each client has  their own distinct set of needs, preferences, and aspirations. That's why we take the time to truly get to know our clients, building relationships based on trust, respect, and empathy.",
+
+        "heading_2": "EXCEPTIONAL",
+
+        "content_2":
+            "Exceptional service is our commitment and passion. We strive to create an experience that goes beyond expectations, providing care that not only meets our clients' needs but also brings comfort, joy, and a sense of well-being. We are dedicated to making a positive impact on the lives of those we serve, ensuring that their time with us is truly exceptional in every way.",
+
+        "heading_3": "ALWAYS OPEN",
+
+        "content_3":
+            `Being "always available" means that we have established systems and processes to ensure that our clients can reach us whenever they require assistance or have concerns. Our lines of communication are open 24/7, allowing clients and their families to contact us easily and without delay.`,
+    },
+
+    // What we do Section:
+    "whatWeDo": {
+
+        "heading": "WHAT WE DO",
+
+        "content":
+            "At Nurtured Home Care Indiana, we specialize in providing a comprehensive selection of non-medical in-home care services customized to your specific requirements. Our utmost commitment is to support individuals in remaining safely in the comfort of their homes while receiving the highest level of care. We firmly believe that delivering care in the appropriate setting and at the right moment is crucial in preserving the dignity of older adults. We recognize that adults and seniors are integral members of our community, and by addressing their personal and home care needs holistically, we ensure their continued active participation and well-being."
+    },
+
+    // Who are we Section:
+    "whoAreWe": {
+
+        "heading": "WHO ARE WE",
+
+        "content":
+            "Nurtured Home Care-Indiana is a licensed, bonded, and insured home care agency serving the State of Indiana, USA. Our team brings together decades of experience in the healthcare field to provide a comprehensive and personalized approach that meets the unique needs of our clients. With a focus on addressing gaps in patient care, we have built a reputation as a premier agency. We offer high-quality in-home care services at affordable prices. Our dedicated staff is available 24/7, ready to serve you and your family. Give us a call today!"
+    },
+
+    // Our Services Section:
+    "ourServices": {
+        "service_1": "PERSONAL CARE ASSISTANCE",
+        "service_2": "COMPANIONSHIP",
+        "service_3": "MEAL PREPARATION",
+        "service_4": "LIGHT HOUSEKEEPING",
+        "service_5": "TRANSPORTATION",
+        "service_6": "RESPITE CARE",
+        "service_7": "SPECIALIZED CARE",
+        "service_8": "MEDICATION MANAGEMENT",
+        "service_9": "AND MORE",
+    },
+
+    // Contact Section:
+    "contacts": {
+        "heading": "Schedule your FREE assessment today",
+
+        "content":
+            "At Nurtured Home Care Indiana, we understand and value each person's unique care needs. Contact us today to explore tailored home care solutions for older adults in your life. Share your details, and we'll promptly assess your needs and schedule a complimentary care assessment. For immediate assistance, please call us, and we'll be ready to help promptly."
+    },
+
+    // Footer Section:
+    "footer": {
+        "brandName": "NURTURED HOME CARE",
+        "address": "3525 Lake Avenue, Suite 1 and 2, Fort Wayne, Indiana, 46805",
+        "phone": "Tel: 260-418-9911",
+        "emailInfo": "Email: info@nhcindiana.com/ timeka@nhcindiana.com",
+        "copyRight": "©2023 by Nurtured Home Care Indiana"
+    },
+
+    // About Page:
+    "aboutUs": {
+        "heading": "About Us",
+        "content":
+            `We are a rising home care agency based in Fort Wayne, Indiana, dedicated to providing specialized and exceptional home care services. With a team of competent caregivers and a commitment to delivering compassionate care, we strive to offer the highest level of support that your loved ones deserve.
+            At Nurtured Home Care-Indiana, we believe that exceptional care should be accessible to all. We are committed to offering affordable prices without compromising on the quality of our services. Our goal is to provide peace of mind to both clients and their families, knowing that their loved ones are receiving top-notch care at a reasonable cost.
+            We take pride in our team of competent caregivers who are carefully selected, undergo thorough background checks, and receive ongoing training to ensure they meet the highest standards of care. Their dedication, empathy, and expertise enable us to deliver exceptional care that goes beyond the physical needs of our clients. We understand the importance of emotional support and companionship, and we strive to create a compassionate and supportive environment for every individual under our care.
+            We are proud to serve the Fort Wayne community and surrounding areas, making a positive impact on the lives of individuals and their families. With our specialized home care services, competent caregivers, and affordable prices, we are dedicated to providing the exceptional care that your loved ones deserve.
+            Contact us today to learn more about our services and how we can support you and your family in your home care needs.
+        `
+    },
+
+    // Our Mission Section:
+    "ourMission": {
+        "heading": "Our Mission",
+        "content":
+            "Our mission is to provide personalized, high-quality care and support to individuals in their homes, enhancing their overall well-being and quality of life. Key aspects include client-centered care, promoting independence and dignity, employing skilled and compassionate staff, prioritizing safety and well-being, ensuring continuity of care, fostering collaboration and communication, and providing education and support. Nurtured Home Care aims to enable individuals to live with dignity and independence by offering compassionate, personalized, and professional care services."
+    },
+
+    // Our Vision Section:
+    "ourVision": {
+        "heading": "Our Vision",
+        "content":
+            "Our vision is to become the leading home care agency in Fort Wayne, Indiana, and the surrounding areas, known for providing exceptional personalized care that enables individuals to thrive in the familiarity and comfort of their homes. We imagine a future where every client receives compassionate support, preserves their independence, and experiences an improved quality of life. Through our unwavering dedication to excellence, innovation, and active community involvement, we aim to establish ourselves as the trusted partner in caring for and nurturing the well-being of our clients and their families."
+    },
+
+    // Core Value Section
+    "coreValue": {
+        "heading": "Core Values",
+        "coreValue_1":
+            "Compassion: Demonstrating heartfelt empathy and care for clients, acknowledging their unique needs, and treating them with genuine kindness and respect.",
+        "coreValue_2":
+            "Integrity: Upholding moral principles, practicing honesty, transparency, and trustworthiness in all interactions with clients, their families, and fellow healthcare professionals.",
+        "coreValue_3":
+            "Excellence: Striving for exceptional care delivery by upholding rigorous standards, continuously enhancing practices, and providing highly skilled and competent caregivers.",
+        "coreValue_4":
+            "Client-Centered Approach: Placing clients at the core of care provision, customizing services to meet their specific needs, preferences, and goals, and actively involving them in decision-making processes.",
+        "coreValue_5":
+            "Independence and Dignity: Respecting and advocating for clients' autonomy and self-worth, empowering them to exercise choice and maintain control over their lives to the greatest extent possible.",
+        "coreValue_6":
+            "Collaboration: Fostering close collaboration among clients, their families, and healthcare professionals involved in their care, promoting open and effective communication, and working together as a unified team to achieve optimal outcomes.",
+        "coreValue_7":
+            "Safety and Well-being: Prioritizing clients' safety and overall well-being by implementing comprehensive safety measures, stringent infection control protocols, and creating a secure environment for care provision.",
+        "coreValue_8":
+            "Continuity of Care: Establishing enduring relationships with clients, ensuring consistent and dependable care over an extended period, and facilitating smooth transitions in care as circumstances evolve.",
+        "coreValue_9":
+            "Professionalism: Demonstrating a professional demeanor at all times, adhering to ethical guidelines, respecting privacy and confidentiality, and complying with regulations and industry standards.",
+        "coreValue_10":
+            "Lifelong Learning: Emphasizing continuous education and training for caregivers, remaining current with best practices, the latest research findings, and technological advancements to provide the utmost quality of care.",
+    },
+
+
+    // Services Page:
+    "services": {
+        "brandName": "NURTURED HOME CARE",
+        "subHeading":
+            "At Nurtured Home Care, we offer compassionate home care services with friendly staff based in Fort Wayne, Indiana USA. Our team is dedicated to providing quality care and support to elderly and disabled individuals, and we strive to ensure that all our clients receive the highest standard of care. We are committed to providing personalized and respectful services to each and every one of our clients."
     }
+
 }
 
 
